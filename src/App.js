d0761b8e337736a6ba0fc54ff856dab3b947251f
canSir21 <alex.mwaura@moringaschool.com>
2022-09-22 09:56:55 +0300

    refactor table

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
 import  Container  from './Container';
 import StudentTable from './StudentTable';
-
-
+import studentData from "./util/studentData"
 
 function App() {
   return (
     <Container>
-      <StudentTable />
+      <StudentTable studentArr={studentData()} />
     </Container>
   );
 }
